04f4b731ca5b493ed347ec6f5c0e84ae8383e5d5
Risky Nugraha <muhammadrisky83@gmail.com>
2023-09-24 20:08:41 +0700

    done

---
+++ week5/pointer.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void clearscr(void)
+{
+#ifdef _WIN32
+  system("cls");
+#elif defined(unix) || defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__))
+  system("clear");
+#else
+#error "OS not supported."
+#endif
+}
+
+// Fungsi pass-by-value
+void swapByValue(int num1, int num2)
+{
+  // x++; // Menambah nilai salinan x
+  int temp = num1;
+  num1 = num2;
+  num2 = temp;
+}
+
+// Fungsi pass-by-address
+void swadByAdrress(int *num1, int *num2)
+{
+  int temp = *num1;
+  *num1 = num2;
+  *num2 = temp;
+}
+
+int main()
+{
+  clearscr();
+  int num1 = 55;
+  int num2 = 77;
+  printf("PASS BY VALUE\n");
+  printf("Nilai num sebelum pemanggilan fungsi: %d\n", num1);
+
+  swapByValue(&num1, &num2); // Memanggil fungsi pass-by-value
+
+  printf("Nilai num setelah pemanggilan fungsi: %d\n", num1);
+
+  printf("\n\nPASS BY REFERENCE\n");
+  printf("Nilai num sebelum pemanggilan fungsi: %d\n", num2);
+
+  swapByAddress(&num1, &num2); // Memanggil fungsi pass-by-address dengan menyertakan alamat num
+
+  printf("Nilai num setelah pemanggilan fungsi: %d\n", num2);
+
+  return 0;
+}
